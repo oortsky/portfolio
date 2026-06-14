61a4520bd8f4dc4fabf1b30892046bd9aaa9f146
Bayu Aprio Pamungkas <oortsky@proton.me>
2026-06-15 00:32:54 +0700

    test: endpoint api

--- api/[[...route]].ts
+++ api/[[...route]].ts
@@ -1,70 +1,25 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { getCookie, setCookie } from "hono/cookie";
 
-export const config = {
-  runtime: "edge"
-};
+const app = new Hono().basePath("/api");
 
-const env = {
-  githubClientId: process.env.GITHUB_CLIENT_ID!,
-  githubClientSecret: process.env.GITHUB_CLIENT_SECRET!,
-  githubRedirectUri: process.env.GITHUB_REDIRECT_URI!,
-  appOrigin:
-    process.env.NODE_ENV === "production"
-      ? "https://oortsky.vercel.app"
-      : "http://localhost:5173"
-};
-
-const app = new Hono();
-
-function popupResponse(payload: Record<string, unknown>, targetOrigin: string) {
-  const message = JSON.stringify(payload);
-  const origin = JSON.stringify(targetOrigin);
-
-  return `
-    <html>
-      <body>
-        <script>
-          window.opener?.postMessage(${message}, ${origin});
-          window.close();
-        </script>
-      </body>
-    </html>
-  `;
-}
-
-app.get("/api/auth", c => {
-  const state = crypto.randomUUID();
-
-  setCookie(c, "oauth_state", state, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "Lax",
-    maxAge: 600,
-    path: "/"
-  });
-
-  const authURL = new URL("https://github.com/login/oauth/authorize");
-
-  authURL.searchParams.set("client_id", env.githubClientId);
-  authURL.searchParams.set("redirect_uri", env.githubRedirectUri);
-  authURL.searchParams.set("scope", "repo user");
-  authURL.searchParams.set("state", state);
-
-  return c.redirect(authURL.toString());
-});
-
-app.get("/api/auth/callback", async c => {
-  return c.text("Callback...");
-});
-
-app.get("/auth/test", c => c.text("test"));
-
-app.get("*", (c) => {
+app.get("/", c => {
   return c.json({
-    path: c.req.path,
+    success: true,
+    message: "API Running"
   });
 });
 
-export default handle(app);
+app.get("/auth", c => {
+  return c.text("Auth");
+});
+
+app.get("/auth/callback", c => {
+  return c.text("Callback");
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
